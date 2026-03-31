a19b251ea805092c174f2d74a96223c975dea63d
liangonzzz <lian.rippeth@gmail.com>
2026-03-31 00:45:31 -0500

    Database Codigos

---
+++ database/migrations/1 Normas_de_la_db.sql
@@ -0,0 +1,12 @@
+/* ... */
+/*
+En esta sección se almacena el código correspondiente a la base de datos.
+
+Cualquier modificación realizada debe registrarse con un número consecutivo
+(001, 002, 003, etc.) para mantener un control de versiones adecuado.
+
+Es importante documentar todos los cambios, incluso los más mínimos, ya que
+esto permite identificar errores y facilitar la reversión a versiones anteriores
+en caso de ser necesario.
+*/
+/* ... */
